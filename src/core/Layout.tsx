408e4cec9d73dc4200fe4573c9ff1999ca641e75
Balaj Marius <balajmarius93@gmail.com>
2021-03-10 15:01:10 +0200

    GlobalFont -> GlobalFonts

--- src/core/Layout.tsx
+++ src/core/Layout.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "styled-components"
 import theme from "../helpers/theme"
 
 import GlobalStyles from "./GlobalStyles"
-import GlobalFonts from "./GlobalFont.js"
+import GlobalFonts from "./GlobalFonts"
 
 interface Props {
   children: JSX.Element | JSX.Element[]
